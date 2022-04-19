8797af9ae66191e1379cd92d0a01818e50390316
kimpham301 <ptp23@txstate.edu>
2022-04-19 15:15:11 -0500

    Add toggle to see contact info

--- src/BreweryData.js
+++ src/BreweryData.js
@@ -3,7 +3,10 @@ import "./BreweryData.css";
 
 export default function BreweryData(props){
     const {name,street, city, brewery_type, phone, website_url} = props
-
+    const  [toggle,setToggle] = useState(false)
+    function showContacts(){
+        setToggle(!toggle)
+    }
 return(
     <div className="container">
     <div className="info">
@@ -14,7 +17,15 @@ return(
         <li>Street: {street ? street :"N/A"}</li>
         <li>City: {city}</li>
         </ul>
-    </div>
+    {toggle && 
+            <div className="contact-info">
+                <div>Phone: {phone ?
+                 phone.slice(0,3) + "-" + phone.slice(3,6) + "-" + phone.slice(6)
+                 : "N/A"}</div>
+                <div>Website: {website_url ? website_url : "N/A"}</div>
+                </div>}
+            </div>
+        <div className="toggle" onClick={showContacts}>+</div>
     </div>
 )
 }
