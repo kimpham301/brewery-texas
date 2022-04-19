6905e90fe658fe2ccd1bbec7694f1fa3a368995c
kimpham301 <ptp23@txstate.edu>
2022-04-19 17:45:11 -0500

    sort options

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const [cityFilter, setCityFilter] = useState('')
 const state = `texas`;
 function oneDay(){
   var date = new Date().toLocaleDateString();
-  if(localStorage.brewery_date == date)
+  if(localStorage.brewery_date === date)
   return false;
   localStorage.brewery_date = date
   return true;
@@ -34,10 +34,11 @@ useEffect(()=>{
         )
       .catch(err=>console.log("Error: ", err))}
 }, []);
-
+/*Create a set of cities */
 const uniqueValue= new Set(data.map(v=>v.city))
   return (
     <div className="page">
+      
        <input
       className = "search-input"
       placeholder="Search by Name"
@@ -48,18 +49,20 @@ const uniqueValue= new Set(data.map(v=>v.city))
     <div className="brewery-page">
     <select className="city-select" onChange={(f)=>setCityFilter(f.target.value)}>
         <option value="">All cities</option>
-        {Array(...uniqueValue).map(value1=>
+        {Array(...uniqueValue).sort((a,b)=>{return a>b ? 1 :-1}).map(value1=>
         <option key={value1} value= {value1}>{value1}</option>
           )}
       
       </select>
-      {data.filter((city)=>{
+      {/*Filter data by city*/
+      data.filter((city)=>{
         if(cityFilter===""){
           return city
         } else if (city.city === cityFilter){
           return city
         }
       })
+      /*Filter data by searchInput*/
       .filter((name)=>{
         if(searchInput===""){
           return name
@@ -67,8 +70,10 @@ const uniqueValue= new Set(data.map(v=>v.city))
           return name
         }
       })
+      /*Sort data alphabetically A-Z*/
       .sort((a,b)=>{
         return a.name >b.name ? 1 : -1;
+      /*Display Data*/
       }).map((brewery) => {
         return(
           <BreweryData key={brewery.id} {...brewery}/>
