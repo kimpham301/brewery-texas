54c4ea421a01b9f6ba518221d40311aed408f737
kimpham301 <ptp23@txstate.edu>
2022-04-19 15:21:48 -0500

    Add sort and search function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BreweryData from "./BreweryData";
 
 export default function App() {
   const [data, setData] = useState([]);
+  const [searchInput, setSearchInput] = useState('')
   const state = "texas";
   async function fetchData(){
     let response =  await fetch(`https://api.openbrewerydb.org/breweries?by_state=${state}`)
@@ -20,8 +21,23 @@ export default function App() {
 
   return (
     <div className="page">
+      <input
+      className = "search-input"
+      placeholder="Search by Name"
+      onChange={(e)=>setSearchInput(e.target.value)}
+      />
      <div className="brewery-page">
-       {data.map((brewery)=>{
+       {data.filter((name)=>{
+        if(searchInput===""){
+          return name
+        } else if (name.name.toLowerCase().includes(searchInput.toLowerCase())){
+          return name
+        }
+      })
+      .sort((a,b)=>{
+        return a.name >b.name ? 1 : -1;
+      })
+      .map((brewery)=>{
          return(
            <BreweryData key={brewery.id} {...brewery} />)
        })}
