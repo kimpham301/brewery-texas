9572388822b9bb66f3d7f8a554d5ee5146dac445
kimpham301 <ptp23@txstate.edu>
2022-04-19 15:37:35 -0500

    Edit info, page's title and icon

--- src/App.js
+++ src/App.js
@@ -1,47 +1,66 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect} from 'react';
 import './App.css';
-import BreweryData from "./BreweryData";
+import BreweryData from './BreweryData';
 
-export default function App() {
-  const [data, setData] = useState([]);
-  const [searchInput, setSearchInput] = useState('')
-  const [cityFilter, setCityFilter] = useState('')
-  const state = "texas";
-  async function fetchData(){
-    let response =  await fetch(`https://api.openbrewerydb.org/breweries?by_state=${state}`)
+export default function App(){
+const [data, setData] = useState([]);
+const [searchInput, setSearchInput] = useState('')
+const [cityFilter, setCityFilter] = useState('')
+const state = `texas`;
+function oneDay(){
+  var date = new Date().toLocaleDateString();
+  if(localStorage.brewery_date == date)
+  return false;
+  localStorage.brewery_date = date
+  return true;
+}
+async function fetchData(){
+    let response = await fetch(`https://api.openbrewerydb.org/breweries?by_state=${state}`)
     let data = await response.json();
-    return data;
-  }
-  useEffect(()=>{
-    fetchData()
-    .then(data =>{
-      setData(data)}
-    )
-    .catch(err=>console.log("Error: ",err))
-  }, []);
-  const uniqueValue= new Set(data.map(v=>v.city))
+    return data
+}
+useEffect(()=>{
+  fetchData()
+    .then(data=> {
+      setData(data)
+      console.log(data)}
+      )
+    .catch(err=>console.log("Error: ", err))
+    if (oneDay()) {
+      fetchData()
+      .then(data=> {
+        setData(data)
+        console.log(data)}
+        )
+      .catch(err=>console.log("Error: ", err))}
+}, []);
+
+const uniqueValue= new Set(data.map(v=>v.city))
   return (
     <div className="page">
-      <input
+       <input
       className = "search-input"
       placeholder="Search by Name"
       onChange={(e)=>setSearchInput(e.target.value)}
       />
-     <div className="brewery-page">
-       <select onChange={(f)=>setCityFilter(f.target.value)}>
+      
+
+    <div className="brewery-page">
+    <select className="city-select" onChange={(f)=>setCityFilter(f.target.value)}>
         <option value="">All cities</option>
         {Array(...uniqueValue).map(value1=>
         <option key={value1} value= {value1}>{value1}</option>
           )}
-          </select>
-       {data.filter((city)=>{
+      
+      </select>
+      {data.filter((city)=>{
         if(cityFilter===""){
           return city
         } else if (city.city === cityFilter){
           return city
         }
       })
-       .filter((name)=>{
+      .filter((name)=>{
         if(searchInput===""){
           return name
         } else if (name.name.toLowerCase().includes(searchInput.toLowerCase())){
@@ -50,14 +69,13 @@ export default function App() {
       })
       .sort((a,b)=>{
         return a.name >b.name ? 1 : -1;
+      }).map((brewery) => {
+        return(
+          <BreweryData key={brewery.id} {...brewery}/>
+        )
       })
-      .map((brewery)=>{
-         return(
-           <BreweryData key={brewery.id} {...brewery} />)
-       })}
-     </div>
-    </div>
-  );
+    }
+ </div>
+  </div>
+  )
 }
-
-
